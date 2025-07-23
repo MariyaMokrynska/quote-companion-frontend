9815b23e5fbc1e5df4a1971f8a49757d60a5710a
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-07-22 17:10:45 -0700

    cleaned index.html and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
 import { useEffect, useState } from 'react'
 import { supabase } from './supabaseClient'
 import './App.css'
@@ -47,7 +11,7 @@ function App() {
   useEffect(() => {
     const fetchQuotes = async () => {
       const { data, error } = await supabase
-        .from('quote') // ✅ Use singular table name
+        .from('quote')
         .select('*')
         .order('created_at', { ascending: false })
 
