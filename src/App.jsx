364dd613d0277b5cef2ed6c3604ae8da7b6880d3
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-07-28 02:19:17 -0700

    changed vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/landingPage" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/mycollections" element={<MyCollections />} />
       </Routes>
     </Router>
