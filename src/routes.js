49a481b91f854c83798b6373ab82c8dfb97e9e73
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-08-08 07:36:52 -0700

    Adds Dashboard and Favorites nav links to Sidebar

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,52 @@
+// const routes = [
+//   {
+//     path: "/myquotes",
+//     name: "My Quotes",
+//     // icon: "nc-icon nc-chart-bar-32",
+//   },
+//   {
+//     path: "/addquote",
+//     name: "Add Quote",
+//     // icon: "nc-icon nc-layout-11",
+//   },
+//   {
+//     path: "/mood-mirror",
+//     name: "Mood Mirror",
+//     // icon: "nc-icon nc-key-25",
+//   },
+//   {
+//     path: "/mycollections",
+//     name: "My Collections",
+//     // icon: "nc-icon nc-book-bookmark",
+//   },
+// ];
+
+// export default routes;
+
 const routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+  },
   {
     path: "/myquotes",
     name: "My Quotes",
-    // icon: "nc-icon nc-chart-bar-32",
   },
   {
     path: "/addquote",
     name: "Add Quote",
-    // icon: "nc-icon nc-layout-11",
   },
   {
     path: "/mood-mirror",
     name: "Mood Mirror",
-    // icon: "nc-icon nc-key-25",
   },
   {
     path: "/mycollections",
     name: "My Collections",
-    // icon: "nc-icon nc-book-bookmark",
+  },
+  {
+    path: "/favorites",
+    name: "Favorites",
   },
 ];
 
