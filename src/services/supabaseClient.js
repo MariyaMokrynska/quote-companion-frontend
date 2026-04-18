1303a68755d91f6f24492f1992ea3d8ec0ead9e5
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2026-04-18 12:49:42 -0700

    fixed supabaseClient.js file

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+/* import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
@@ -10,3 +10,14 @@ if (!supabase) {
 }
 
 export { supabase }
+ */
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase environment variables")
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
