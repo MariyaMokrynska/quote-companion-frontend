fa3e0e6ec90930dd0d175918be3d88205aeca34a
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2026-04-20 00:44:03 -0700

    returning Login logic

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -49,8 +49,7 @@ function Login() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        // redirectTo: `${window.location.origin}/dashboard`, 
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: `${window.location.origin}/dashboard`, 
       },
     });
   };
